b27e7c4115b30e1b62961a436ab2e059be1bcb58
Toai-Unity <toainv@wolffungame.com>
2023-03-28 11:36:04 +0700

    Initial commit

---
+++ quantum_code/quantum.code/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿#if !UNITY_2018_4_OR_NEWER
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("quantum.code")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("quantum.code")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("fbf32099-b197-4ab9-8e5a-b44d9d3750bd")]
+[assembly: AssemblyVersion("2.1.5")]
+[assembly: AssemblyFileVersion("2.1.5")]
+[assembly: AssemblyInformationalVersion("2.1.5 Stable 1144 2.1/develop (4c516225e)")]
+#endif
---
+++ quantum_code/quantum.console.runner/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("quantum.console.runner")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("quantum.console.runner")]
+[assembly: AssemblyCopyright("Copyright © Exit Games 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("b2c620e4-13f8-4ecd-a7f0-88724216e90c")]
+[assembly: AssemblyVersion("2.1.5")]
+[assembly: AssemblyFileVersion("2.1.5")]
+[assembly: AssemblyInformationalVersion("2.1.5 Stable 1144 2.1/develop (4c516225e)")]
